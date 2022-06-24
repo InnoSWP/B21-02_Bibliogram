64a1b25f81d7952990afd7651bf1c31687fce3bb
Yaroslav Sokolov <sokoslav1707@gmail.com>
2022-06-25 02:30:03 +0400

    test: testing system

--- tests/functional/test_filtration.py
+++ tests/functional/test_filtration.py
@@ -1,2 +1,4 @@
 def test_filtration(test_client):
     test_client().post("/publications", data={"sort": "Publication Date"})
+    response = test_client.get("/publications")
+    assert  b"Publication Date" in response
